f1ea2b010ad9e2951b7cbf0018042455ac2d2639
niiquash <qua16006@byui.edu>
2023-06-10 23:13:51 -0600

    Initial commit: completed getall and getsinglemovie endpoints, starting a post operation for reviews endoint

---
+++ src/main/java/dev/ammon/movies/Review.java
@@ -0,0 +1,19 @@
+package dev.ammon.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+    public Review(String body) {
+        this.body = body;
+    }
+}
