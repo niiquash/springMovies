15691e777170cd96c9fcef495a63b5eb4fc5ab4b
niiquash <qua16006@byui.edu>
2023-06-14 19:39:34 -0600

    error handling

--- src/main/java/dev/ammon/movies/MoviesApplication.java
+++ src/main/java/dev/ammon/movies/MoviesApplication.java
@@ -1,9 +1,12 @@
 package dev.ammon.movies;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class MoviesApplication {
 
 	public static void main(String[] args) {
